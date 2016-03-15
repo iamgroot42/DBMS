5a804c71a1a99cd82c19bb88286645c29411ec62
Divam Gupta <divamgupta@gmail.com>
2016-03-15 16:34:12 +0530

    basic class structure created

--- README.md
+++ README.md
@@ -2,4 +2,12 @@
 DBMS course project 
 
 
-patata
+Parts :
+
+Packages :
+	Models - all the DB classes
+	API - all the core java API
+
+	Server - http REST Wrapper for the java APIs using spark
+
+	UI - A standalone UI to consume the API
--- my-app/src/main/java/com/iiitd/dbms/app/App.java
+++ my-app/src/main/java/com/iiitd/dbms/app/App.java
@@ -14,12 +14,64 @@ import java.sql.Statement;
  * Hello world!
  *
  */
+
+
+class DMManager{
+
+
+    	String dbUrl = "jdbc:mysql://localhost/mydatabase";
+	    String dbClass = "com.mysql.jdbc.Driver";
+	    // String query = "Select distinct(table_name) from INFORMATION_SCHEMA.TABLES";
+	    String username = "root";
+	    String password = "";
+
+	    DMManager( String dbUrl  , String username ,   String password   )
+	    {
+	    	this.dbUrl = dbUrl;
+	    	this.username = username;
+	    	this.password = password;
+	    }
+
+
+}
+
+class User extends DMManager{
+	String name;
+	String passwordHash;
+	String userId;
+
+	// initialiilze the DB table
+	public static void initTable()
+	{
+
+	}
+
+	// take the id and add  it .... if exists then uddate it 
+	public void save()
+	{
+
+	}
+
+	public void delete()
+	{
+
+	}
+
+	public static ArrayList<User> getAll()
+	{
+
+	}
+
+
+}
+
+
 public class App 
 {
 	// start spark server
     public static void main(String[] args) {
 
-    	 String dbUrl = "jdbc:mysql://localhost/mydatabase";
+    	String dbUrl = "jdbc:mysql://localhost/mydatabase";
 	    String dbClass = "com.mysql.jdbc.Driver";
 	    String query = "Select distinct(table_name) from INFORMATION_SCHEMA.TABLES";
 	    String username = "root";
@@ -32,10 +84,11 @@ public class App
 	            username, password);
 	        Statement statement = connection.createStatement();
 	        ResultSet resultSet = statement.executeQuery(query);
+
 	        while (resultSet.next()) {
-	        String tableName = resultSet.getString(1);
-	        System.out.println("Table name : " + tableName);
-	        }
+		        String tableName = resultSet.getString(1);
+		        System.out.println("Table name : " + tableName);
+	    	}
 	        connection.close();
 	    } catch (ClassNotFoundException e) {
 	        e.printStackTrace();
