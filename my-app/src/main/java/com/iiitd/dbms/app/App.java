01e02c73f0804eaa3af3e3815146b8963ceafcc0
Divam Gupta <divamgupta@gmail.com>
2016-03-15 11:51:50 +0530

    mysql support added

--- my-app/src/main/java/com/iiitd/dbms/app/App.java
+++ my-app/src/main/java/com/iiitd/dbms/app/App.java
@@ -3,6 +3,13 @@ package com.iiitd.dbms.app;
 import static spark.Spark.*;
 
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  * Hello world!
  *
@@ -11,6 +18,33 @@ public class App
 {
 	// start spark server
     public static void main(String[] args) {
+
+    	 String dbUrl = "jdbc:mysql://localhost/mydatabase";
+	    String dbClass = "com.mysql.jdbc.Driver";
+	    String query = "Select distinct(table_name) from INFORMATION_SCHEMA.TABLES";
+	    String username = "root";
+	    String password = "";
+
+	     try {
+
+	        Class.forName(dbClass);
+	        Connection connection = DriverManager.getConnection(dbUrl,
+	            username, password);
+	        Statement statement = connection.createStatement();
+	        ResultSet resultSet = statement.executeQuery(query);
+	        while (resultSet.next()) {
+	        String tableName = resultSet.getString(1);
+	        System.out.println("Table name : " + tableName);
+	        }
+	        connection.close();
+	    } catch (ClassNotFoundException e) {
+	        e.printStackTrace();
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    }
+
+
+
         get("/hello", (req, res) -> "Hello World");
     }
 }
